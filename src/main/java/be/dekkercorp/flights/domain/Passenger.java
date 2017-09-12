5a9a4070b6ea166ee9481ae4a6e92220c6f2aa96
Vontjen <vontjen@gmail.com>
2017-09-12 16:02:20 +0200

    commit from class

--- src/main/java/be/dekkercorp/flights/domain/Passenger.java
+++ src/main/java/be/dekkercorp/flights/domain/Passenger.java
@@ -1,8 +1,14 @@
 package be.dekkercorp.flights.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
 public class Passenger {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String firstName;
     private String lastName;
